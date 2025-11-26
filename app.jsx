6ec9121f33e8ab21ebe34f0e15e7fb392f5d692e
jangir-monika <astravyon11@gmail.com>
2025-11-27 00:23:58 +0530

    Create app.jsx

---
+++ app.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚀 React Deployment Test Successful!</h1>
+      <p>Your React app is working live 🎉</p>
+    </div>
+  );
+}
+
+export default App;
